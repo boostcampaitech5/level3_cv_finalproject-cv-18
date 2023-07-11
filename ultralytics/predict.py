df3eaca23b4c6550537f65d3abf391c60a4e3f00
IsSong <80191837+0523kevin@users.noreply.github.com>
2023-07-11 14:12:09 +0900

    Revert "[feat] YOLO v8"

--- ultralytics/predict.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("/opt/ml/ultralytics/object-detection/100epoch/weights/best.pt")
-
-source = "/opt/ml/ultralytics/data/Cars0.png"
-
-model.predict(source, save=True, imgsz=640, conf=0.5)
-
-
