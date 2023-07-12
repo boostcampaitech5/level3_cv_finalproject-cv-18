bd0e6ab5ec1107cf6426ef196817c6b8732663bb
jemin7709 <jemin7709@naver.com>
2023-07-12 08:48:28 +0000

    [feat] 동영상 OCR API v1
    - 10초 정도의 짧은 동영상만 처리 가능
    related to: #1, #2

--- app/server.py
+++ app/server.py
@@ -1,19 +1,99 @@
+import io
+import os
+import tempfile
+import time
+import warnings
+from datetime import timedelta
+from typing import Tuple
+
+import cv2
 import uvicorn
 from fastapi import FastAPI, File
 from fastapi.responses import JSONResponse
+from ocr.inference import inference, load_text_recognition_model
+from PIL import Image
+from ultralytics import YOLO
+
+warnings.filterwarnings("ignore")
 
 app = FastAPI()
 
 
-@app.post("/ocr")
-def test(file: bytes = File(...)):
-    # ocr결과 리스트
-    output = {
-        "자동차1": {"coordinate": [(150, 1000), (1500, 2000)], "OCR": "경기5느 2784"},
-        "자동차2": {"coordinate": [(300, 450), (1000, 800)], "OCR": "경기51거 2824"},
-    }
+model = YOLO("/opt/ml/serving/best.pt")
+ocr_model = load_text_recognition_model(save_model="/opt/ml/serving/best_accuracy.pth", device="cuda")
+
+
+# input: 이미지, output: [(x, y, x, y), (x, y, x, y)](좌표)
+def detection(image):
+    image = image.convert("RGB")
+    results = model(image)
+    plate_list = []
+    boxes = results[0].boxes.cpu().numpy()
+    for box in boxes:
+        item = tuple([int(xy) for xy in box.xyxy[0].astype(int)])
+        plate_list.append(item)
+    return plate_list
+
+
+# input: 단일 좌표, 이미지
+# output: OCR 결과
+def ocr(image, box: Tuple) -> str:
+    image = image.convert("L")
+    cropped_box = image.crop((*box,))
+    ocr_result = inference(model=ocr_model, img_array=cropped_box, device="cuda")
+    return str(ocr_result)
+
+
+# 이미지 바운딩박스, OCR 결과 계산
+@app.post("/image")
+def image(file: bytes = File(...)):
+    image = Image.open(io.BytesIO(file))
+    plate_list = detection(image)
+    output = {}
+
+    for i, item in enumerate(plate_list):
+        ocr_result = ocr(image, item)
+        output[f"car{i}"] = {"coordinate": [(item[0], item[1]), (item[2], item[3])], "OCR": str(ocr_result)}
+
     return JSONResponse(content=output)
 
+
+# 비디오 바운딩박스 OCR 결과 계산
+@app.post("/video")
+def video(file: bytes = File(...)):
+    # start = time.time()
+    num_frame = 0
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        f.write(file)
+        temp_path = f.name
+
+    capture = cv2.VideoCapture(temp_path)
+    output_list = {}
+    while capture.isOpened():
+        ret, frame = capture.read()
+        if not ret:
+            break
+        frame = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        output = {}
+
+        if num_frame == 1:
+            pass
+
+        plate_list = detection(frame)
+        for i, item in enumerate(plate_list):
+            ocr_result = ocr(frame, item)
+            output[f"car{i}"] = {"coordinate": [(item[0], item[1]), (item[2], item[3])], "OCR": str(ocr_result)}
+        output_list[f"frame{num_frame}"] = output
+        num_frame += 1
+
+    capture.release()
+    os.unlink(temp_path)
+    # print(timedelta(seconds=time.time() - start))
+    return JSONResponse(content=output_list)
+
+
+# database -> sqlquery("insert into !~~~#$@$")
+# image -> GCS
 
 if __name__ == "__main__":
     uvicorn.run(app)
--- app/ui.py
+++ app/ui.py
@@ -1,21 +1,31 @@
+import io
 import tempfile
 from ast import literal_eval
+from copy import deepcopy
+from datetime import datetime
 
+import av
 import cv2
 import numpy as np
 import requests
-
-# import request
 import streamlit as st
+from PIL import Image, ImageDraw, ImageFont
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 # interact with FastAPI endpoint
-backend = "http://127.0.0.1:8000/segmentation"
+backend_image = "http://127.0.0.1:8000/image"
+backend_video = "http://127.0.0.1:8000/video"
 
 
-def process(image, server_url: str):
+def image_process(image, server_url: str):
     m = MultipartEncoder(fields={"file": ("filename", image, "image/png")})
+    r = requests.post(server_url, data=m, headers={"Content-Type": m.content_type}, timeout=8000)
 
+    return r
+
+
+def video_process(video, server_url: str):
+    m = MultipartEncoder(fields={"file": ("filename", video, "video/mp4")})
     r = requests.post(server_url, data=m, headers={"Content-Type": m.content_type}, timeout=8000)
 
     return r
@@ -23,10 +33,6 @@ def process(image, server_url: str):
 
 # construct UI layout
 st.title("블랙박스 번호판 찾기")
-
-# st.write(
-#     """영상 또는 이미지를 업로드하여 번호판 인식"""
-# )  # description and instructions
 
 input = st.file_uploader("영상 또는 이미지를 업로드하여 번호판 인식")  # image upload widget
 if input:
@@ -38,13 +44,22 @@ if st.button("번호판 찾기"):
         if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
             # detection 결과를 받아서 bounding box를 쳐줌
             with st.spinner("로딩중..."):
-                segments = process(input, backend)
+                segments = image_process(input, backend_image)
                 my_dict = literal_eval(segments.content.decode("utf-8"))
                 print(my_dict)
                 file_bytes = np.asarray(bytearray(input.read()), dtype=np.uint8)
                 opencv_image = cv2.imdecode(file_bytes, 1)
+                original_image = deepcopy(opencv_image)
                 # 가능한 색 -> 하나만 해도 될거같다
-                # pallete = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+                pallete = [
+                    (255, 0, 0),
+                    (0, 255, 0),
+                    (0, 0, 255),
+                    (200, 200, 200),
+                    (200, 100, 100),
+                    (100, 200, 100),
+                    (100, 100, 200),
+                ]
                 i = 0
                 for key, value in my_dict.items():
                     for k, v in value.items():
@@ -52,7 +67,7 @@ if st.button("번호판 찾기"):
                             # cv2.rectangle(opencv_image, v[0], v[1], pallete[i], 3)
                             cv2.rectangle(opencv_image, v[0], v[1], (255, 0, 0), 3)
                             i += 1
-                st.image(opencv_image, use_column_width=True)
+                st.image(opencv_image, use_column_width=True, channels="BGR")
             # OCR 결과
             for key, value in my_dict.items():
                 with st.container():
@@ -60,34 +75,78 @@ if st.button("번호판 찾기"):
                     for k, v in value.items():
                         # 각 bounding box에 해당하는 영역을 crop해 출력
                         if k == "coordinate":
-                            cropped_img = opencv_image[v[0][1] : v[1][1], v[0][0] : v[1][0]]
-                            print([v[0][0], v[1][0], v[0][1], v[1][1]], cropped_img.shape, opencv_image.shape)
-                            col1.image(cropped_img, use_column_width=True)
+                            cropped_img = original_image[v[0][1] : v[1][1], v[0][0] : v[1][0]]
+                            # print([v[0][0], v[1][0], v[0][1], v[1][1]], cropped_img.shape, opencv_image.shape)
+                            col1.image(cropped_img, use_column_width=True, channels="BGR")
                         # 각 bounding box에 해당하는 번호판을 crop된 이미지 옆에 출력
                         elif k == "OCR":
                             col2.text(f"해당 번호판의 번호는 {v}입니다.")
         # 입력값이 영상일 경우
         elif filename.endswith(".avi") or filename.endswith(".mp4"):
-            # st.video(input.read())
+            output = video_process(input, backend_video)
+            video_dict = literal_eval(output.content.decode("utf-8"))
             frameST = st.empty()
+            img_frame_list = []
 
             def play_video(file):
                 cap = cv2.VideoCapture(file.name)
                 while True:
                     ret, frame = cap.read()
-                    if frame is None:
+                    if frame is not None:
+                        img_frame_list.append(frame)
+                        height, width, layers = frame.shape
+                        size = (width, height)
+                        fps = cap.get(cv2.CAP_PROP_FPS)
+                        total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                    else:
                         break
-                    frameST.image(frame, channels="BGR")
-                cap.release()
+                return size, fps, total_frame
 
             video = tempfile.NamedTemporaryFile(delete=False)
             video.write(input.read())
+            size, fps, total_frame = play_video(video)
+            output_memory_file = io.BytesIO(input.read())
+            width = size[0]
+            height = size[1]
+            n_frmaes = int(fps)
+            output = av.open(output_memory_file, "w", format="mp4")
+            stream = output.add_stream("h264", str(fps))
+            stream.width = width
+            stream.height = height
+            # stream.pix_fmt = 'yuv444p'
+            stream.pix_fmt = "yuv420p"
+            stream.options = {"crf": "17"}
+            # 적당한 폰트 크기가 얼마지?
+            font = ImageFont.truetype("fonts/MaruBuri-Bold.ttf", int(height / 36))
+            for i in range(total_frame):
+                img = img_frame_list[i]
+                for keys, values in video_dict.items():
+                    if keys == f"frame{i}":
+                        for key, value in values.items():
+                            for k, v in value.items():
+                                if k == "coordinate":
+                                    # print(v[0], v[1])
+                                    cv2.rectangle(img, v[0], v[1], (255, 0, 0), 3)
+                                    x, y = v[0][0], v[0][1]
+                                elif k == "OCR":
+                                    img_pil = Image.fromarray(img)
+                                    draw = ImageDraw.Draw(img_pil)
+                                    draw.text((x, y - int(height / 36) - 2), v, (255, 0, 0), font=font)
+                                    img = np.array(img_pil)
 
-            play_video(video)
+                frame = av.VideoFrame.from_ndarray(img, format="bgr24")
+                packet = stream.encode(frame)
+                output.mux(packet)
+            packet = stream.encode(None)
+            output.mux(packet)
+            output.close()
 
-            # if st.button("재생"):
-            #     play_video(video)
-            # 입력
+            output_memory_file.seek(0)
+            with open("output.mp4", "wb") as f:
+                f.write(output_memory_file.getbuffer())
+            video_file = open("output.mp4", "rb")
+            video_bytes = video_file.read()
+            st.video(video_bytes)
     else:
         # handler: no image
         st.text("이미지를 업로드 한 후 번호판 찾기를 눌러주세요")
